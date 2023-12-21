8c433ae9c34d9d55ad63de4198a2ef14c6410e49
Kayla C <itskaychay@gmail.com>
2023-12-21 17:27:55 -0500

    added offline

--- src/api.js
+++ src/api.js
@@ -1,21 +1,28 @@
 // src/api.js
 
 import mockData from "./mock-data";
+import NProgress from "nprogress";
 
-/**
- *
- * @param {*} events:
- * The following function should be in the “api.js” file.
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
 
-export const extractLocations = (events) => {
-  const extractedLocations = events.map((event) => event.location);
-  const locations = [...new Set(extractedLocations)];
+  const tokenCheck = accessToken && (await checkToken(accessToken));
 
-  return locations;
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://uy7vebtz9b.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
 };
 
 const checkToken = async (accessToken) => {
@@ -24,6 +31,40 @@ const checkToken = async (accessToken) => {
   );
   const result = await response.json();
   return result;
+};
+
+// this function will fetch the list of events
+export const getEvents = async () => {
+  // NProgress.start();
+  if (window.location.href.startsWith("http://localhost")) {
+    return mockData;
+  }
+
+  // access local storage when user is offline
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url =
+      "https://uy7vebtz9b.execute-api.us-east-1.amazonaws.com/dev/api/get-events" +
+      "/" +
+      token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
+      return result.events;
+    } else {
+      return null;
+    }
+  }
 };
 
 const removeQuery = () => {
@@ -54,47 +95,9 @@ const getToken = async (code) => {
   return access_token;
 };
 
-// this function will fetch the list of events
-export const getEvents = async () => {
-  if (window.location.href.startsWith("http://localhost")) {
-    return mockData;
-  }
+export const extractLocations = (events) => {
+  const extractedLocations = events.map((event) => event.location);
+  const locations = [...new Set(extractedLocations)];
 
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url =
-      "https://uy7vebtz9b.execute-api.us-east-1.amazonaws.com/dev/api/get-events" +
-      "/" +
-      token;
-    const response = await fetch(url);
-    const result = await response.json();
-    if (result) {
-      return result.events;
-    } else {
-      return null;
-    }
-  }
-};
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem("access_token");
-
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const response = await fetch(
-        "https://uy7vebtz9b.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const result = await response.json();
-      const { authUrl } = result;
-      return (window.location.href = authUrl);
-    }
-    return code && accessToken;
-  }
+  return locations;
 };
